5393ca86da5a01df74003446162f890943fde67e
semanurakkuzu <semanurakkuzu@gmail.com>
2023-04-10 14:38:37 +0300

    added background image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Cards from "./components/Cards";
+import Provinces from "./components/Provinces";
 
 function App() {
   return (
-    <div className="App">
-        <Cards></Cards>
+    <div className="h-screen bg-cover bg-center bg-[url('./images/background1.jpg')]">
+      <div className="pt-10 text-zinc-500">
+        <p className="text-center text-7xl pb-10">Weather</p>
+       
+        </div>
     </div>
   );
 }
