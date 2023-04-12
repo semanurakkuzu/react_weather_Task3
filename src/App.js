592f4c6364257d841152705b459bb5dc083e278a
semanurakkuzu <semanurakkuzu@gmail.com>
2023-04-12 11:30:26 +0300

    added weather image

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import Cards from "./components/Cards";
-import Provinces from "./components/Provinces";
+
+import Cards from './components/Cards'
+import Provinces from './components/Provinces'
+import {SelectProvider} from './context/SelectProvince'
 
 function App() {
   return (
-    <div className="h-screen bg-cover bg-center bg-[url('./images/background1.jpg')]">
-      <div className="pt-10 text-zinc-500">
-        <p className="text-center text-7xl pb-10">Weather</p>
-        <Provinces></Provinces>
-        <Cards></Cards>
+    <SelectProvider>
+      <div className="h-screen bg-cover bg-center  bg-[url('./images/background3.jpg')]">
+        <div className="pt-10 text-zinc-500">
+          <p className="text-center text-7xl pb-10">Weather</p>
+          <Provinces></Provinces>
+          <Cards></Cards>
         </div>
-    </div>
-  );
+      </div>
+    </SelectProvider>
+  )
 }
 
-export default App;
+export default App
