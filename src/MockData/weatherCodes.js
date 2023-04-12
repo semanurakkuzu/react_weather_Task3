1c367d5e10951c6eb4287db9002d608f80e51a30
semanurakkuzu <semanurakkuzu@gmail.com>
2023-04-12 11:30:43 +0300

    added weather codes

---
+++ src/MockData/weatherCodes.js
@@ -0,0 +1,87 @@
+import rainNight from '../images/rain_night.gif'
+import rain from '../images/rain.gif'
+import clearSky from '../images/clear_sky.gif'
+import clearSkyNight from '../images/clear_sky_night.gif'
+import fewClouds from '../images/few_clouds.gif'
+import scatteredClouds from '../images/scattered_clouds.gif'
+import brokenClouds from '../images/broken_clouds.gif'
+import showerRain from '../images/shower_rain.gif'
+import thunderstorm from '../images/thunderstorm.gif'
+import fewCloudsNight from '../images/few_clouds_night.gif'
+import snow from '../images/snow.gif'
+import mist from '../images/mist.gif'
+
+export default {
+  '01d': {
+    title: 'Clear Sky',
+    icon: clearSky
+  },
+  '01n': {
+    title: 'Clear Sky',
+    icon: clearSkyNight
+  },
+  '02d': {
+    title: 'Few Clouds',
+    icon: fewClouds
+  },
+  '02n': {
+    title: 'Few Clouds Night',
+    icon: fewCloudsNight
+  },
+  '03d': {
+    title: 'scattered Clouds',
+    icon: scatteredClouds
+  },
+  '03n': {
+    title: 'scattered Clouds',
+    icon: scatteredClouds
+  },
+  '04d': {
+    title: 'broken Clouds',
+    icon: brokenClouds
+  },
+  '04n': {
+    title: 'broken Clouds',
+    icon: brokenClouds
+  },
+  '09d': {
+    title: 'shower Rain',
+    icon: showerRain
+  },
+  '09n': {
+    title: 'shower Rain',
+    icon: showerRain
+  },
+  '10d': {
+    title: 'rain',
+    icon: rain
+  },
+  '10n': {
+    title: 'rain Night',
+    icon: rainNight
+  },
+  '11d': {
+    title: 'thunderstorm',
+    icon: thunderstorm
+  },
+  '11n': {
+    title: 'thunderstorm',
+    icon: thunderstorm
+  },
+  '13d': {
+    title: 'snow',
+    icon: snow
+  },
+  '13n': {
+    title: 'snow',
+    icon: snow
+  },
+  '50d': {
+    title: 'mist',
+    icon: mist
+  },
+  '50n': {
+    title: 'mist',
+    icon: mist
+  }
+}
